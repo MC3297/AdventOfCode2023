36bfca50ee795450c5b7693d16a8dd1f4ac6bfc8
MC3297 <97566000+MC3297@users.noreply.github.com>
2023-12-02 00:25:07 -0500

    Add files via upload

---
+++ d2p2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class d2p2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("txt.in"));
+		PrintWriter pw = new PrintWriter("txt.out");
+        int ans = 0;
+        for (int z = 0; z < 100; z++) {
+            String s = r.readLine();
+            s = s.replaceAll(",", "");
+            String pp = s.split(": ")[1];
+            String[] spl = pp.split("; ");
+            int fr = 0, fb = 0, fg = 0;
+            for (String i: spl) {
+                int rr = 0, bb = 0, gg = 0;
+                String[] tmp = i.split(" ");
+                for (int j = 0; j < tmp.length; j+=2) {
+                    int n = Integer.parseInt(tmp[j]);
+                    if (tmp[j+1].length() == 3) rr = n;
+                    if (tmp[j+1].length() == 4) bb = n;
+                    if (tmp[j+1].length() == 5) gg = n;
+                }       
+                fr = Math.max(fr, rr);
+                fg = Math.max(fg, gg);
+                fb = Math.max(fb, bb);
+            }
+            ans += fr*fg*fb;
+        }
+        pw.println(ans);
+        pw.close();
+        r.close();
+    }
+}
